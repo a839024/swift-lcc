eeaf1443868d02ae2a403c9350a654a0be2d0846
LCC <lcc@1906-16.local>
2017-04-23 12:13:21 +0800

    swift3 basic type format practice

---
+++ 20170423/20170423-playgroud.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//轉型
+print(Int(Int8.min) - 1)
+
+//var 變數名稱: 型態 = 值
+var Allen_LCC: String = "name"
+
+print("Hello World")
+//terminator 定義結尾
+print("LCC", terminator:".\n")
+
+//在字串中放置變數 \(variable)
+var year = 2017
+print("今年是\(year - 1911)年")
+print("Hello \\LCC\\\n我是\t\"xxx\"")
+
+//字串相加
+var hello = "hello "
+var world = "world"
+print(hello + world)
