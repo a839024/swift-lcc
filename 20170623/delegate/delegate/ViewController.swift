adc07219acb80cc89e1021470272961eac47f772
a839024 <a839024@gmail.com>
2017-06-23 21:18:33 +0800

    Fcu ios class 0623

---
+++ 20170623/delegate/delegate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  delegate
+//
+//  Created by RTC15 on 2017/6/23.
+//  Copyright © 2017年 a839024. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
